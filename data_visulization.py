2ceb6ffc55e20a01c3b8fcdfe04b6b76e6b98341
cp3mvp <lq2156@columbia.edu>
2015-05-19 09:25:03 -0400

    data_visulization.py

---
+++ data_visulization.py
@@ -0,0 +1,54 @@
+df = pd.read_csv("/Users/liqi/Downloads/titanic/data/train.csv") 
+df = df.drop(['Ticket','Cabin'], axis=1)
+# Remove NaN values
+df = df.dropna() 
+fig = plt.figure(figsize=(18,12), dpi=1600)
+a = 0.65
+# Step 1
+ax1 = fig.add_subplot(341)
+df.Survived.value_counts().plot(kind='bar', color="blue", alpha=a)
+ax1.set_xlim(-1, len(df.Survived.value_counts()))
+plt.title("Step. 1")
+
+# Step 2
+ax2 = fig.add_subplot(345)
+df.Survived[df.Sex == 'male'].value_counts().plot(kind='bar',label='Male')
+df.Survived[df.Sex == 'female'].value_counts().plot(kind='bar', color='#FA2379',label='Female')
+ax2.set_xlim(-1, 2)
+plt.title("Step. 2 \nWho Survied? with respect to Gender."); plt.legend(loc='best')
+
+ax3 = fig.add_subplot(346)
+(df.Survived[df.Sex == 'male'].value_counts()/float(df.Sex[df.Sex == 'male'].size)).plot(kind='bar',label='Male')
+(df.Survived[df.Sex == 'female'].value_counts()/float(df.Sex[df.Sex == 'female'].size)).plot(kind='bar', color='#FA2379',label='Female')
+ax3.set_xlim(-1,2)
+plt.title("Who Survied proportionally?"); plt.legend(loc='best')
+
+
+# Step 3
+ax4 = fig.add_subplot(349)
+female_highclass = df.Survived[df.Sex == 'female'][df.Pclass != 3].value_counts()
+female_highclass.plot(kind='bar', label='female highclass', color='#FA2479', alpha=a)
+ax4.set_xticklabels(["Survived", "Died"], rotation=0)
+ax4.set_xlim(-1, len(female_highclass))
+plt.title("Who Survived? with respect to Gender and Class"); plt.legend(loc='best')
+
+ax5 = fig.add_subplot(3,4,10, sharey=ax1)
+female_lowclass = df.Survived[df.Sex == 'female'][df.Pclass == 3].value_counts()
+female_lowclass.plot(kind='bar', label='female, low class', color='pink', alpha=a)
+ax5.set_xticklabels(["Died","Survived"], rotation=0)
+ax5.set_xlim(-1, len(female_lowclass))
+plt.legend(loc='best')
+
+ax6 = fig.add_subplot(3,4,11, sharey=ax1)
+male_lowclass = df.Survived[df.Sex == 'male'][df.Pclass == 3].value_counts()
+male_lowclass.plot(kind='bar', label='male, low class',color='lightblue', alpha=a)
+ax6.set_xticklabels(["Died","Survived"], rotation=0)
+ax6.set_xlim(-1, len(male_lowclass))
+plt.legend(loc='best')
+
+ax7 = fig.add_subplot(3,4,12, sharey=ax1)
+male_highclass = df.Survived[df.Sex == 'male'][df.Pclass != 3].value_counts()
+male_highclass.plot(kind='bar', label='male highclass', alpha=a, color='steelblue')
+ax7.set_xticklabels(["Died","Survived"], rotation=0)
+ax7.set_xlim(-1, len(male_highclass))
+plt.legend(loc='best')
